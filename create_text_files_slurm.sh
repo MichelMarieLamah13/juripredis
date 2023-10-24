7890355e2cb0db96769562a1a6e511a6c7c8183a
mmlamah <lamahmichelmarie@gmail.com>
2023-10-24 09:33:37 +0200

    Create text file slurm GPU unlimited time

--- create_text_files_slurm.sh
+++ create_text_files_slurm.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=rcc_ctf
+#SBATCH --output=create_text_files_output.log
+#SBATCH --error=create_text_files_error.log
+#SBATCH --partition=gpu
+#SBATCH --time=0-00:00
 
 conda activate rcc
 
