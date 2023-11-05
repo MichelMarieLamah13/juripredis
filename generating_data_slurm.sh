ea8d13e1e703e72f5519e3f046cb52ccba84e72f
miguel <lamahmichelmarie@gmail.com>
2023-11-05 21:05:50 +0100

    05 Nov 2023 21:05

--- generating_data_slurm.sh
+++ generating_data_slurm.sh
@@ -10,13 +10,17 @@ source /etc/profile.d/conda.sh
 conda activate rcc
 
 # python3 create_judilibre_v_data.py
-awk 'NR == 1 {print; next} FNR > 1' \
-judilibre_v_embeddings_0_975394.tsv judilibre_v_embeddings_975395_1950789.tsv \
-judilibre_v_embeddings_1950790_2926184.tsv judilibre_v_embeddings_2926185_3901579.tsv \
-judilibre_v_embeddings_3901580_4876974.tsv judilibre_v_embeddings_4876975_5852369.tsv \
-judilibre_v_embeddings_5852370_6827764.tsv judilibre_v_embeddings_6827765_7803159.tsv \
-judilibre_v_embeddings_7803160_8778554.tsv judilibre_v_embeddings_8778555_9753949.tsv \
-> judilibre_v/judilibre_v_data.csv
+
+# awk 'NR == 1 {print; next} FNR > 1' \
+# judilibre_v_embeddings_0_975394.tsv judilibre_v_embeddings_975395_1950789.tsv \
+# judilibre_v_embeddings_1950790_2926184.tsv judilibre_v_embeddings_2926185_3901579.tsv \
+# judilibre_v_embeddings_3901580_4876974.tsv judilibre_v_embeddings_4876975_5852369.tsv \
+# judilibre_v_embeddings_5852370_6827764.tsv judilibre_v_embeddings_6827765_7803159.tsv \
+# judilibre_v_embeddings_7803160_8778554.tsv judilibre_v_embeddings_8778555_9753949.tsv \
+# > judilibre_v/judilibre_v_data.csv
+
+awk 'BEGIN {FS=OFS=","} NR==1 {$2="chunk"} {print}' \
+judilibre_v/judilibre_v_data.csv > judilibre_v/judilibre_v_data_new.csv
 
 
 conda deactivate
